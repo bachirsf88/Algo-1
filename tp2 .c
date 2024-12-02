c1afc0924aad4ac4724bc076e96cb5b72580b536
bachirsf88 <bachirsf6@gmail.com>
2024-12-02 23:25:15 +0100

    Update tp2 .c

--- tp2 .c
+++ tp2 .c
@@ -1,1 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// سؤال الاول 
+char *ChargerChaine(int N) {
+    // حجز مكان في ذاكرة 
+    char *chaine = (char *)malloc((N + 1) * sizeof(char));
+    printf("Entrez la chaine de caracter ne deppasser pas la taille maximal %d : ", N);
+    fgets(chaine, N + 1, stdin); 
+ //  for (int i = 0; chaine[i] != '\0'; i++) {
+//         if (chaine[i] == '\n') {
+//             chaine[i] = '\0';
+//             break;
+//         }
+//     }
+    return chaine;
+}
+// سؤال الثاني 
+// نحسب طول شان تاعي 
+int Longueur(char *ch) {
+    int l = 0;
+    while (ch[l] != '\0') {
+        l++;
+    }
+    return l;
+}
+// سؤال الثالث 
+// نعمرو شان تاعي في جدول
+void ChargerTab(char *p, char Tab[]) {
+    int i = 0;
+    while (p[i] != '\0') {
+        Tab[i] = p[i];
+        i++;
+    }
+    Tab[i] = '\0'; 
+}
+
+// سؤال الرابع 
+// نقلب قيم جدول 
+void InverserTab(char Tab[], char T[], int m) {
+    for (int i = 0; i < m; i++) {
+        T[i] = Tab[m - i - 1]; 
+    }
+    T[m] = '\0'; 
+}
+// سؤال الخامس
+// اضهار جدول
+void AfficherTab(char Tab[], int m) {
+    for (int i = 0; i < m; i++) {
+        printf("%c", Tab[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int N;
+    
+   
+    printf("Entrez la taille maximale de la chaîne : ");
+    scanf("%d", &N);
+    getchar(); 
+    
+  
+    char *chaine = ChargerChaine(N);
+   
+   
+        printf("La longueur de la chaîne est : %d\n", Longueur(chaine));
+        
+      
+        char Tab[N + 1];
+        ChargerTab(chaine, Tab);
+        
+        
+        printf("Le tableau contient : ");
+        AfficherTab(Tab, Longueur(Tab));
+        
+       
+        char T[N + 1];
+        InverserTab(Tab, T, Longueur(Tab));
+        
+        
+        printf("Le tableau inversé est : ");
+        AfficherTab(T, Longueur(T));
+        
+        
+
+  return 0;
+}
